334fd15f06de2c4d9f717f7b711cb42bdf79a3d0
Felipe Rastelli <77105763+feliperastelli@users.noreply.github.com>
2025-05-06 01:17:40 -0300

    Add files via upload

---
+++ api_model/main.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import pickle
+import uvicorn
+
+# Classe para input via JSON
+class LyricsInput(BaseModel):
+    lyrics: str
+
+# Carrega o modelo treinado
+with open("../models/model.pkl", "rb") as f:
+    model = pickle.load(f)
+
+app = FastAPI()
+
+@app.post("/predict")
+def predict_genre(data: LyricsInput):
+    prediction = model.predict([data.lyrics])
+    return {"genre": prediction[0]}
+
+# Apenas para execução local
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
---
+++ streamlit_app/app.py
@@ -0,0 +1,41 @@
+import streamlit as st
+import requests
+
+if "lyrics_input" not in st.session_state:
+    st.session_state.lyrics_input = ""
+
+def clear_input():
+    st.session_state.lyrics_input = ""
+
+st.title("Classificador de Gênero Musical 🎵")
+st.write("Digite a letra da música abaixo:")
+
+col1, col2 = st.columns(2)
+
+with col2:
+    if st.button("Limpar"):
+        clear_input()
+
+lyrics_input = st.text_area("Letra da música", key="lyrics_input")
+
+with col1:
+    if st.button("Classificar"):
+        if not st.session_state.lyrics_input.strip():
+            st.warning("Insira uma letra válida!")
+        else:
+            try:
+                response = requests.post(
+                    "http://localhost:8000/predict",  # Se local: "http://localhost:8000/predict"
+                    json={"lyrics": st.session_state.lyrics_input}
+                )
+                if response.status_code == 200:
+                    genre = response.json()["genre"]
+                    st.success(f"Gênero classificado: **{genre}**")
+                else:
+                    st.error("Erro na resposta da API")
+            except Exception as e:
+                st.error(f"Erro ao conectar com a API: {e}")
+
+
+
+                 
